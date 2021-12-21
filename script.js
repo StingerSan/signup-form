28e4d926edaae389b3717312a9bce4e3c75935a1
StingerSan <stinger@int.pl>
2021-12-21 19:57:14 +0100

    working on submit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const button = document.querySelector("button")
-const input = document.querySelectorAll("input")
+const inputItems = document.querySelectorAll("input")
+const labelItems = document.querySelectorAll("label")
+
+const submit = () => {
+    inputItems.forEach((inputItems) => {
+        inputItems.classList.add("activeInput");
+        inputItems.removeAttribute("placeholder");
+    })
+    labelItems.forEach((labelItems)=> {
+        labelItems.classList.add("active");
+    })
+
+}
+
+button.addEventListener("click", submit)
