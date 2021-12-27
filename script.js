749cd2d1ae202bfc1d53b3b31ac244a52c4dd02f
Stinger <stinger@int.pl>
2021-12-27 18:45:55 +0100

    working on submit

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const button = document.querySelector("button")
-const inputItems = document.querySelectorAll("input")
+const inputItem1 = document.getElementById("FirstName")
+const inputItem2 = document.getElementById("LastName")
+const inputItem3 = document.getElementById("EmailAddress")
+const inputItem4 = document.getElementById("Password")
 const labelItems = document.querySelectorAll("label")
 
 const submit = () => {
-    inputItems.forEach((inputItems) => {
-        inputItems.classList.add("activeInput");
-        inputItems.removeAttribute("placeholder");
-    })
-    labelItems.forEach((labelItems)=> {
-        labelItems.classList.add("active");
-    })
+    if (inputItem1.value === "") {
+        inputItem1.classList.add("activeInput");
+        inputItem1.removeAttribute("placeholder");
+    } else
+        inputItem1.classList.remove("activeInput");
+
+    if (inputItem2.value === "") {
+        inputItem2.classList.add("activeInput");
+        inputItem2.removeAttribute("placeholder");
+    } else
+        inputItem2.classList.remove("activeInput");
+
+    if (inputItem3.value === "") {
+        inputItem3.classList.add("activeInput");
+        inputItem3.removeAttribute("placeholder");
+    } else
+        inputItem3.classList.remove("activeInput");
+
+    if (inputItem4.value === "") {
+        inputItem4.classList.add("activeInput");
+        inputItem4.removeAttribute("placeholder");
+    } else
+        inputItem4.classList.remove("activeInput");
+
 
 }
-
 button.addEventListener("click", submit)
